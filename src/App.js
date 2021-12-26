5795e9d16fc1465c11ee8ae4b5633d0fd6396650
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-12-25 22:51:38 -0500

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, Routes} from 'react-router-dom';
 import { Layout, Typography, Space} from 'antd';
 //import Navbar from './components/Navbar';
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
 import './App.css';
 const App =() => {
     return (
@@ -12,7 +12,29 @@ const App =() => {
                 <Navbar />
             </div>
             <div className="main">
+                <Layout>
+                    <div className="routes">
+                    
+                        <Routes>
+                            {/* home page path */}
+                            <Route exact path="/" element={<Homepage />} />
+                            
 
+                            {/*path to the exchange */}
+                            <Route exact path="/exchanges" element={<Exchanges />} />
+                            
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}/>
+                            
+
+                            <Route exact path="/crypto/:coinId" element={<CryptoDetails />}/>
+                            
+
+                            <Route exact path="/news" element={<News />}/>
+                            
+                        </Routes>
+                    
+                    </div>
+                </Layout>
             </div>
             <div className="footer">
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
+/*
 ReactDOM.render(
     <Router>
     <App />
     </Router>, 
-    document.getElementById('root'));
+    document.getElementById('root'));*/
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
