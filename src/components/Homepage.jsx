3d8a5ae50cc6e261d9e78059d7732bd404bbbbe9
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-12-26 23:46:23 -0500

    data now fetch's properly and renders for homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,25 +2,47 @@ import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic} from 'antd';
 import { Link } from 'react-router-dom';
-import { useGetCryptosQuery} from '../services/cryptoApi';
-const { Title} = Typography;
+import { useGetCryptosQuery } from '../services/cryptoApi';
+const { Title } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching} = useGetCryptosQuery();
-    console.log("data:"+ JSON.stringify(data));
-    return (
-        <div>
-            <Title level = {2} className="heading">Global Crypto Stats</Title>
-            <Row>
-                {/* for value attribute 5 is a placeholder will fetch real data*/}
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>  {/* <== This Col will take up 12 spaces which half of the width of the screen as there are 24 spaces in Ant as opposed to 12 in bootstrap*/}
-                <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
-                <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
-            </Row> 
-        </div>
-    )
+    
+    
+    const {data, loading, error, isFetching} = useGetCryptosQuery();
+    //console.log("data:"+ JSON.stringify(data));
+    const globalStats = data?.data?.stats;
+    //globalStats = JSON.stringify(globalStats);
+    //console.log("DATA: "+globalStats);
+
+    if (isFetching) return 'Loading ...';
+    
+    /**
+     * If globalStates has a value it will render the values else render message to user that the data could not be retrieived. 
+     */
+    if (globalStats) {
+        return (
+            <div>
+                <Title level = {2} className="heading">Global Crypto Stats</Title>
+                <Row>
+                    {/* for value attribute 5 is a placeholder will fetch real data*/}
+                    <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats ? globalStats.total : "N/A" }/></Col>  {/* globalStats.total <== This Col will take up 12 spaces which half of the width of the screen as there are 24 spaces in Ant as opposed to 12 in bootstrap*/}
+                    <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                    <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+                    <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                    <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+                </Row> 
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h3 className="text-center">Unable to fetch data from server. This could be due to a network or server issue 
+                please try again later</h3>
+                
+            </div>
+        )
+    }
+    
 }
 
 export default Homepage
